686d01e22f51235de12ca8fedb871986a780b847
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2021-04-18 20:32:24 +0530

    Fixed code

--- terraform/install_app.sh
+++ terraform/install_app.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 sudo apt-get update
-wget http://nexus.mygurukulam.org:8081/repository/mild-temper-microservice/pool/s/spinnaker-study/spinnaker-study_1.27_all.deb
-sudo dpkg -i spinnaker-study_1.27_all.deb || true
-sudo apt-get install -f -y
+sudo apt-get install nginx -y
