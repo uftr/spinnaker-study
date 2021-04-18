7542f995f1bbfac13caa5691d42c5d6060f71f7f
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2021-04-18 20:29:13 +0530

    Changed script

--- terraform/install_app.sh
+++ terraform/install_app.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 wget http://nexus.mygurukulam.org:8081/repository/mild-temper-microservice/pool/s/spinnaker-study/spinnaker-study_1.27_all.deb
 sudo dpkg -i spinnaker-study_1.27_all.deb || true
-sudo apt-get install -f 
+sudo apt-get install -f -y
