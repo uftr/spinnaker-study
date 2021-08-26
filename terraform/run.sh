6ae68d2e6353dd103478d6e949f249b3215776f1
Mehul-Sharma <mehul@gisd88.com>
2021-08-26 23:52:22 +0530

    Adding Jenkins Job Images for WIKI

--- terraform/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-TERRAFORM_COMMAND="$1"
-terraform init
-terraform ${TERRAFORM_COMMAND}
-
-# Output the results so we can use it within Spinnaker
-# All outputs must be prefixed with SPINNAKER_PROPERTY
-
-echo "SPINNAKER_PROPERTY_PUBLIC_IP=$(terraform output public_ip | tr -d '"')"
--- tools/github-accounts/github-token-gen.scala
+++
@@ -1,8 +0,0 @@
-val names = Map(
-   "mild-temper" -> "92632b5810d442b81b005573723dd450e85a928d"
-)
-
-names.foreach{case (k,v) =>
-  println(s"- name: $k")
-  println(s"  token: $v")
-}
